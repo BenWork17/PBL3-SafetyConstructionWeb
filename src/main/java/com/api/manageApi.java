880b717d6a4a43b1b3b72b88578c386931ae2abe
Nguyen Van Chuong <nguyenvanchuongwind@gmail.com>
2024-06-06 14:48:39 +0700

    update

--- src/main/java/com/api/manageApi.java
+++ src/main/java/com/api/manageApi.java
@@ -24,8 +24,10 @@ public class manageApi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		/*
+		 * response.getWriter().append("Served at: ").append(request.getContextPath());
+		 */	
+		}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
